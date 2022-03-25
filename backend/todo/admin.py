9c857d140cbe85106ae7386bc6df66ce006ecf2b
Gio Tremari <tremag@rpi.edu>
2022-03-25 01:11:31 -0400

    got black working in vscode time to test precommit hooks on an arbitrary .py file

--- backend/todo/admin.py
+++ backend/todo/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from .models import Todo
 
+
 class TodoAdmin(admin.ModelAdmin):
-    list_display = ('title', 'description', 'completed')
+    list_display = ("title", "description", "completed")
+
 
 # Register your models here.
 
