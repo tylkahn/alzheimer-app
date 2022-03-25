f33787905f80c80d38e75c50aa7bb3ca632be75f
Gio Tremari <tremag@rpi.edu>
2022-03-25 16:53:29 -0400

    Seems like login is configured out of the box for django So now its onto the frontend I'll worry about connecting them later

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -19,9 +19,10 @@ from rest_framework import routers
 from todo import views
 
 router = routers.DefaultRouter()
-router.register(r'todos', views.TodoView, 'todo')
+router.register(r"todos", views.TodoView, "todo")
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/', include(router.urls)),
+    path("admin/", admin.site.urls),
+    path("api/", include(router.urls)),
+    # path("api-auth/", include("rest_framework.urls")),
 ]
