76400761e76a51990b205a63ce17b5a24b181a5e
Jimmers2001 <38543433+Jimmers2001@users.noreply.github.com>
2022-03-25 03:06:08 -0400

    Revert "untracked files?"
    This reverts commit 6d42fb2bffc612bc19925924dc04344fb0f96985.

--- backend/todo/views.py
+++ backend/todo/views.py
@@ -1,14 +1,10 @@
 from django.shortcuts import render
 from rest_framework import viewsets
-from .serializers import ReminderSerializer
-from .models import Reminder
+from .serializers import TodoSerializer
+from .models import Todo
 
 # Create your views here.
 
-# class TodoView(viewsets.ModelViewSet):
-#     serializer_class = TodoSerializer
-#     queryset = Todo.objects.all()
-
-class ReminderView(viewsets.ModelViewSet):
-    serializer_class = ReminderSerializer
-    queryset = Reminder.objects.all()
+class TodoView(viewsets.ModelViewSet):
+    serializer_class = TodoSerializer
+    queryset = Todo.objects.all()
