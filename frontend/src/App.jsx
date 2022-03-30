24e99760cb15cbc21ba6fab8328425605939ce56
tylkahn <tylkahn@gmail.com>
2022-03-30 18:58:14 -0400

    fix duplicate files

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,36 +1,49 @@
 import React, { useState, useEffect } from 'react';
-import Modal from "./components/Modal";
 import axios from "axios";
-import JournalTab from "./components/journal/JournalTab";
-import Gametab from "./components/game/Gametab";
+import ReminderModal from "./components/ReminderModal";
+import Gametab from "./components/game/Gametab"
 import './App.css';
-import { nanoid } from 'nanoid';
+import ReminderTab from './components/reminder/ReminderTab'
 
-// TODO: DELETE THIS LINE
+const BASE_URL = 'http://localhost:8000'
 
 function App() {
-  const [viewCompleted, setViewCompleted] = useState(false);
-
-  const displayCompleted = (status) => {
-    if (status) {
-      setViewCompleted(true);
-    } else {
-      setViewCompleted(false);
+  const [viewTab, setViewTab] = useState("");
+  const [reminderList, setReminderList] = useState([]);
+  const [modal, setModal] = useState(false);
+  const [activeItem, setActiveItem] = useState(
+    {
+      title: "",
+      reminderType: "",
+      date: "",
+      repeating: "",
+      description: "",
+      completed: false,
     }
+  );
+
+  const displayTab = (tabName) => {
+    setViewTab(tabName);
   }
 
   const renderTabList = () => {
     return (
       <div className="nav nav-tabs">
         <span
-          className={viewCompleted ? 'nav-link active' : 'nav-link'}
-          onClick={() => displayCompleted(true)}
+          className={viewTab ? "nav-link active" : "nav-link"}
+          onClick={() => displayTab("reminder")}
+        >
+          Reminder
+        </span>
+        <span
+          className={viewTab ? "nav-link active" : "nav-link"}
+          onClick={() => displayTab("journal")}
         >
           Journal
         </span>
         <span
-          className={viewCompleted ? 'nav-link' : 'nav-link active'}
-          onClick={() => displayCompleted(false)}
+          className={viewTab ? "nav-link" : "nav-link active"}
+          onClick={() => displayTab("game")}
         >
           Game
         </span>
@@ -39,16 +52,132 @@ function App() {
   };
 
   const renderTab = () => {
-    return (//entryList will start empty and become filled through addEntry button presses as the end goal
-      <div className="journaltab">
-        {viewCompleted && <JournalTab entries={
-          [
-            { id: nanoid(4), title: "title1", description: "desc1" },
-            { id: nanoid(4), title: "title2", description: "desc2" },
-          ]
-        } />}
+    return (
+      <div className="tabs">
+        {viewTab == "journal" && <div className="journaltab">
+          <JournalTab entries={
+            [
+              { id: nanoid(4), title: "title1", description: "desc1" },
+              { id: nanoid(4), title: "title2", description: "desc2" },
+            ]
+          } />
+        </div>}
+        {viewTab == "reminder" && <div className="remindertab">
+          <ReminderTab />
+          <main className="container">
+            <h1 className="text-white text-uppercase text-center my-4">Reminder app</h1>
+            <div className="row">
+              <button
+                className="btn btn-primary"
+                onClick={createItem}
+              >
+                Add task
+              </button>
+            </div>
+            <ul className="list-group list-group-flush border-top-0">
+              {renderItems()}
+            </ul>
+            {modal ? (
+              <ReminderModal
+                activeItem={activeItem}
+                toggle={toggle}
+                onSave={handleSubmit}
+              />
+            ) : null}
+          </main>
+        </div>}
+        {viewTab == "game" && <Gametab />}
       </div>
+
     )
+  };
+
+  const handleSubmit = (item) => {
+    console.log("handleSubmit");
+    toggle();
+    console.log(item);
+
+    // NOTE: let it be known that I very much dislike using axios.put and axios.delete
+    if (item.id) {
+      axios
+        .put(BASE_URL + `/api/reminders/${item.id}/`, item)
+        .then(() => refreshList());
+    } else {
+      axios
+        .post(BASE_URL + "/api/reminders/", item)
+        .then(() => refreshList());
+    }
+  };
+
+  const deleteItem = (item) => {
+    axios
+      .delete(BASE_URL + `/api/reminders/${item.id}/`)
+      .then(() => refreshList());
+  };
+
+  const createItem = () => {
+    const item = { title: "", reminderType: "", date: "", repeating: "", description: "", completed: false };
+    console.log("created item")
+    setActiveItem(item);
+    toggle();
+  };
+
+  const editItem = (item) => {
+    setActiveItem(item);
+    console.log(item);
+    toggle();
+  };
+
+  const refreshList = () => {
+    axios
+      .get(BASE_URL + "/api/reminders")
+      .then((res) => {
+        console.log(res);
+        setReminderList(res.data)
+      })
+      .catch((err) => console.log(err));
+  };
+
+  useEffect(() => {
+    refreshList();
+  }, []);
+
+  const toggle = () => {
+    setModal(!modal)
+  };
+
+  const renderItems = () => {
+    const newItems = reminderList.filter(
+      (item) => item.completed == viewCompleted
+    );
+
+    return newItems.map((item) => (
+      <li
+        key={item.id}
+        className="list-group-item d-flex justify-content-between align-items-center"
+      >
+        <span
+          className={`reminder-title mr-2 ${viewCompleted ? "completed-reminder" : ""}`}
+          title={item.description}
+        >
+          {item.title}
+        </span>
+        <span>
+          <button
+            className="btn btn-secondary mr-2"
+            onClick={() => editItem(item)}
+          >
+            Edit
+          </button>
+          <button
+            className="btn btn-danger"
+            onClick={() => deleteItem(item)}
+          >
+            Delete
+          </button>
+        </span>
+      </li>
+    ));
   };
 
   return (
