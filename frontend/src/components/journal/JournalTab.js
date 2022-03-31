890b80dc6282d3c53c43bcbca5376fdb21bb4e7d
Jimmers2001 <james.d.batista@gmail.com>
2022-03-31 14:24:07 -0400

    starter code for file upload

--- frontend/src/components/journal/JournalTab.js
+++ frontend/src/components/journal/JournalTab.js
@@ -18,7 +18,7 @@ class JournalTab extends React.Component {
             }
             */
             entryList: this.props.entries,
-            entryTitle: '',
+            entryTitle: `Entry Title ${this.props.entries.length+1}`,
             entryDescription: '',
             searchText: '',
         };
@@ -70,10 +70,14 @@ class JournalTab extends React.Component {
 
     handleTitleChange = (event) => {
         //event.target.value is what was typed into the text area
-        this.setState({
-            entryTitle: event.target.value,
-        });
-        console.log(this.state.entryTitle);
+        //////in the title: putting a bunch of spaces, typing a character, and deleting the character, 
+            //will add an entry that is titled that deleted character
+        if (event.target.value.trim().length > 0){
+            this.setState({
+                entryTitle: event.target.value,
+            
+            });
+        }
     }
 
     render = () => {
