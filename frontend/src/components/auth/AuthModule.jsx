ec3828a4667f45301e1ff9055e2c7778d4439253
Gio Tremari <tremag@rpi.edu>
2022-03-31 13:58:21 -0400

    login functionality works (for demo purposes) error highlighting and the other fancy stuff is being difficult right now, but there is hardcoded support for one account I'll connect the db after i finish the frontend stuff (will not be done in time for interim release demo)

--- frontend/src/components/auth/AuthModule.jsx
+++ frontend/src/components/auth/AuthModule.jsx
@@ -4,24 +4,67 @@ import LoginForm from "./LoginForm";
 import SignUpForm from "./SignUpForm"; // TODO: maybe refactor and make the login form do both login and sign up
 
 function AuthModule(props) {
-  // const { authInfo, setAuthInfo } = props;
-  const { setAuthInfo } = props;
+  const { authInfo, setAuthInfo } = props;
   const [isActiveLoginForm, setIsActiveLoginForm] = useState(true);
+
   const toggleForm = () => {
     setIsActiveLoginForm(!isActiveLoginForm);
   };
+
+  const handleLoginChange = (event) => {
+    event.preventDefault();
+    const { name, value } = event.target;
+
+    // TODO: THIS VALIDATION IS FOR DEMO PURPOSES ONLY - REPLACE AS SOON AS THE BACKEND IS CONNECTED
+    switch (name) {
+      case "email":
+        value === "test@user.com"
+          ? event.target.setCustomValidity("")
+          : event.target.setCustomValidity(
+              "username or password is incorrect!"
+            );
+        event.target.checkValidity();
+        break;
+      case "password":
+        value === "password123"
+          ? event.target.setCustomValidity("")
+          : event.target.setCustomValidity(
+              "username or password is incorrect!"
+            );
+        event.target.checkValidity();
+        break;
+      default:
+        event.target.checkValidity();
+        break;
+    }
+  };
+
   const handleLoginSubmit = (event) => {
-    console.log("handleLoginSubmit running:");
-    console.log(event);
-    setAuthInfo({ isLoggedIn: true });
+    event.preventDefault();
+
+    // event.target.checkValidity();
+    event.target.classList.remove("was-validated");
+    // debugger;
+    if (event.target.reportValidity()) {
+      setAuthInfo({ ...authInfo, isLoggedIn: true });
+    } else {
+      event.target.classList.add("was-validated");
+    }
+    // return event.target.reportValidity();
+
     // TODO: send credentials to server (which sends them to db), set auth info based on server response
   };
+
   const handleSignUpSubmit = () => {
     // TODO: send data to server, set auth info based on server response
   };
 
   return isActiveLoginForm ? (
-    <LoginForm handleSubmit={handleLoginSubmit} toggleForm={toggleForm} />
+    <LoginForm
+      handleSubmit={handleLoginSubmit}
+      handleChange={handleLoginChange}
+      toggleForm={toggleForm}
+    />
   ) : (
     <SignUpForm handleSubmit={handleSignUpSubmit} toggleForm={toggleForm} />
   );
--- frontend/src/components/auth/LoginForm.jsx
+++ frontend/src/components/auth/LoginForm.jsx
@@ -3,50 +3,60 @@ import React from "react";
 import PropTypes from "prop-types";
 
 function LoginForm(props) {
-  const { handleSubmit, toggleForm } = props;
+  const { handleSubmit, handleChange, toggleForm } = props;
   // NOTE: possible optimization (almost certainly unnecessary):
   // const submitFunction = useCallback((e) => {
   //   handleSubmit(e);
   // }, []);
   return (
-    <form
-      className="login-form-noVal"
-      onSubmit={(e) => handleSubmit(e)}
-      noValidate
-    >
-      <div className="form-group">
-        <label htmlFor="email">Email address</label>
-        <input
-          id="email"
-          type="email"
-          className="form-control"
-          placeholder="name@example.com"
-        />
-        <label htmlFor="password">Password</label>
-        <input
-          id="password"
-          type="password"
-          className="form-control"
-          placeholder="very_secure_password"
-        />
-        <button type="submit" className="btn btn-primary">
-          Login
-        </button>
-        <button
-          type="button"
-          id="signUpLink"
-          className="btn btn-link"
-          onClick={() => toggleForm()}
-          aria-label="Account Sign Up Button"
-        >
-          Don&#39t have an account? Click here to sign up!
-        </button>
-      </div>
-    </form>
+    <>
+      <div className="invalid-feedback">Username or password is incorrect!</div>
+      <form
+        className="login-form-noVal"
+        onSubmit={(e) => handleSubmit(e)}
+        // noValidate
+      >
+        <div className="form-group">
+          <label htmlFor="email">Email address</label>
+          <input
+            id="email"
+            name="email"
+            type="email"
+            className="form-control mb-2"
+            placeholder="name@example.com"
+            onChange={(e) => handleChange(e)}
+            required
+          />
+          <label htmlFor="password">Password</label>
+          <input
+            id="password"
+            name="password"
+            type="password"
+            className="form-control mb-4"
+            placeholder="totally_legit_and_secure_password_example"
+            onChange={(e) => handleChange(e)}
+            required
+          />
+          <button type="submit" className="btn btn-primary">
+            Login
+          </button>
+          <button
+            type="button"
+            id="signUpLink"
+            className="btn btn-link"
+            onClick={() => toggleForm()}
+            aria-label="Account Sign Up Button"
+          >
+            Don&#39;t have an account? Click here to sign up!
+          </button>
+        </div>
+      </form>
+    </>
   );
 }
 LoginForm.propTypes = {
   handleSubmit: PropTypes.func.isRequired,
+  handleChange: PropTypes.func.isRequired,
   toggleForm: PropTypes.func.isRequired,
 };
 
